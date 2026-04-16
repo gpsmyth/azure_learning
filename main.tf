50ab4d029168e4100301a110d6a69a5f15de849d
Gerry Smyth <gerry.p.smyth@gmail.com>
2026-04-16 15:46:24 +1200

    Adding storage acconut

--- main.tf
+++ main.tf
@@ -22,3 +22,18 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.1.0/24"]
 }
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS" # Not mirroring production.
+
+  https_traffic_only_enabled = true # Default is true as noted in README.md, but explicitly set here for clarity
+
+  tags = {
+    environment = "test"
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "storage_account_name" {
+  description = "storage account name"
+  type        = string
+  default     = "mystorageacct12345"
+}
